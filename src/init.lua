2c9f7fbdff92595efb1c3c6674690fe18b127efa
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-30 18:39:32 +0300

    Module start/update implementation

--- src/init.lua
+++ src/init.lua
@@ -1,9 +1,12 @@
+local RunService = game:GetService("RunService")
+
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 
 local Emulators = {
 	Player = require(script.Emulation.Player),
 }
+local Log = require(script.Tier0.Logger)
 local Carbon = {
 	Tier0 = script:WaitForChild("Tier0"),
 	Emulation = script:WaitForChild("Emulation"),
@@ -21,7 +24,6 @@ local Carbon = {
 	Pools = {
 		RenderUpdate = {},
 	},
-	Log = require(script.Tier0.Logger),
 }
 
 function Carbon:GetPlayer(Name: string)
@@ -47,7 +49,7 @@ function Carbon:RegisterModule(Module: table | ModuleScript)
 	)
 
 	if type(Module) == "table" then
-		self.Log:Log(
+		Log:Log(
 			"WARNING: Registering a module by table reference is not recommended and will remove its key in Carbon.Modules. Provide the ModuleScript instead.",
 			self.Log.InfoType.Warning
 		)
@@ -60,12 +62,29 @@ function Carbon:RegisterModule(Module: table | ModuleScript)
 	end
 
 	if Module["Update"] then
-		table.insert(self.Pools, function(Delta: number)
+		table.insert(self.Pools.RenderUpdate, function(Delta: number)
 			Module:Update(Delta)
 		end)
 	end
 end
 
-function Carbon:Start() end
+function Carbon:Start()
+	Log:Log("Starting Carbon", Log.InfoType.Debug)
+
+	-- Module Start
+	for _, Module in pairs(self.Modules) do
+		if Module["Start"] then
+			Module:Start()
+		end
+	end
+
+	RunService.RenderStepped:Connect(function(DeltaTime: number)
+		for _, Update in pairs(self.Pools.RenderUpdate) do
+			Update(DeltaTime)
+		end
+	end)
+
+	Log:Log("Finished starting Carbon", Log.InfoType.Debug)
+end
 
 return Carbon
