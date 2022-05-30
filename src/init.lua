c49df9bd0bcc80f3309c6de3b20ba32e4d41a342
Haruka <96925398+haruland@users.noreply.github.com>
2022-05-30 18:35:35 +0300

    Carbon Module Registering

--- src/init.lua
+++ src/init.lua
@@ -17,6 +17,11 @@ local Carbon = {
 		UserId = 21450341,
 		AccountAge = 18,
 	}),
+	Modules = {},
+	Pools = {
+		RenderUpdate = {},
+	},
+	Log = require(script.Tier0.Logger),
 }
 
 function Carbon:GetPlayer(Name: string)
@@ -34,5 +39,33 @@ function Carbon:GetPlayer(Name: string)
 			AccountAge = 500,
 		})
 end
+
+function Carbon:RegisterModule(Module: table | ModuleScript)
+	assert(
+		type(Module) == "table" or Module:IsA("ModuleScript"),
+		"The provided module must be a Table or ModuleScript."
+	)
+
+	if type(Module) == "table" then
+		self.Log:Log(
+			"WARNING: Registering a module by table reference is not recommended and will remove its key in Carbon.Modules. Provide the ModuleScript instead.",
+			self.Log.InfoType.Warning
+		)
+		table.insert(self.Modules, Module)
+	else
+		local Key = Module.Name
+		assert(self.Modules[Key], "The module already exists.")
+
+		self.Modules[Key] = require(Module)
+	end
+
+	if Module["Update"] then
+		table.insert(self.Pools, function(Delta: number)
+			Module:Update(Delta)
+		end)
+	end
+end
+
+function Carbon:Start() end
 
 return Carbon
