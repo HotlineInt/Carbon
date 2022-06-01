feea9681467c5334dc81c722c419394d83a31b73
Haruka <96925398+haruland@users.noreply.github.com>
2022-06-02 01:13:55 +0300

    Improve Logger

--- src/Tier0/Logger.lua
+++ src/Tier0/Logger.lua
@@ -10,22 +10,44 @@ local Logger = { InfoType = {
 	Debug = "DEBUG",
 	Exception = "EXCEPTION",
 } }
+local MessageFormat = "[%s][%s] %s"
+
+function Logger:_LogMessage(Prefix: any, Message: any, PrintFunc)
+	local Time = os.date("%H:%M:%S")
+	PrintFunc(string.format(MessageFormat, Time, Prefix, Message))
+end
 
 function Logger:Log(Message: any, Context: InfoType)
 	if not Context then
 		Context = self.InfoType.Info
 	end
-	local Time = os.date("%H:%M:%S")
-
-	local FinalMessage = "[" .. tostring(Time) .. "] " .. "[" .. Context .. "] " .. Message
-
 	if Context == self.InfoType.Info then
-		print(FinalMessage)
+		self:_LogMessage(Context, Message, print)
 	elseif Context == self.InfoType.Warning or Context == self.InfoType.Debug then
-		warn(FinalMessage)
+		self:_LogMessage(Context, Message, warn)
 	elseif Context == self.InfoType.Exception then
-		error(FinalMessage)
+		self:_LogMessage(Context, Message, error)
 	end
+end
+
+function Logger:Info(...)
+	local Message = tostring(...)
+	self:_LogMessage("INFO", Message, print)
+end
+
+function Logger:Warn(...)
+	local Message = tostring(...)
+	self:_LogMessage("WARNING", Message, warn)
+end
+
+function Logger:Debug(...)
+	local Message = tostring(...)
+	self:_LogMessage("DEBUG", Message, warn)
+end
+
+function Logger:Exception(...)
+	local Message = tostring(...)
+	self:_LogMessage("EXCEPTION", Message, error)
 end
 
 return Logger
