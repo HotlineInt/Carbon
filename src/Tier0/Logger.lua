3caf3e9e03bfbade00b9e1da814c0c6a9e907462
Haruka <96925398+haruland@users.noreply.github.com>
2022-06-02 12:32:15 +0300

    Improve logger with Source support

--- src/Tier0/Logger.lua
+++ src/Tier0/Logger.lua
@@ -10,44 +10,49 @@ local Logger = { InfoType = {
 	Debug = "DEBUG",
 	Exception = "EXCEPTION",
 } }
-local MessageFormat = "[%s][%s] %s"
+local MessageFormat = "[%s][%s][%s] %s"
 
-function Logger:_LogMessage(Prefix: any, Message: any, PrintFunc)
+function Logger:_LogMessage(Prefix: any, Source, Message: any, PrintFunc)
 	local Time = os.date("%H:%M:%S")
-	PrintFunc(string.format(MessageFormat, Time, Prefix, Message))
+	PrintFunc(string.format(MessageFormat, Time, Source, Prefix, Message))
 end
 
 function Logger:Log(Message: any, Context: InfoType)
 	if not Context then
 		Context = self.InfoType.Info
 	end
+	local Source = getfenv(0).script.Name
 	if Context == self.InfoType.Info then
-		self:_LogMessage(Context, Message, print)
+		self:_LogMessage(Context, Source, Message, print)
 	elseif Context == self.InfoType.Warning or Context == self.InfoType.Debug then
-		self:_LogMessage(Context, Message, warn)
+		self:_LogMessage(Context, Source, Message, warn)
 	elseif Context == self.InfoType.Exception then
-		self:_LogMessage(Context, Message, error)
+		self:_LogMessage(Context, Source, Message, error)
 	end
 end
 
 function Logger:Info(...)
 	local Message = tostring(...)
-	self:_LogMessage("INFO", Message, print)
+	local Source = getfenv(0).script.Name
+	self:_LogMessage("INFO", Source, Message, print)
 end
 
 function Logger:Warn(...)
 	local Message = tostring(...)
-	self:_LogMessage("WARNING", Message, warn)
+	local Source = getfenv(0).script.Name
+	self:_LogMessage("WARNING", Source, Message, warn)
 end
 
 function Logger:Debug(...)
 	local Message = tostring(...)
-	self:_LogMessage("DEBUG", Message, warn)
+	local Source = getfenv(0).script.Name
+	self:_LogMessage("DEBUG", Source, Message, warn)
 end
 
 function Logger:Exception(...)
 	local Message = tostring(...)
-	self:_LogMessage("EXCEPTION", Message, error)
+	local Source = getfenv(0).script.Name
+	self:_LogMessage("EXCEPTION", Source, Message, error)
 end
 
 return Logger
